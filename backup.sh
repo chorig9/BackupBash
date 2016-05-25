ec2ed24297d0408f50f23436dff6cbb0f1260a7e
Igor <chorig9@gmail.com>
2016-05-25 14:32:45 +0200

    new options

--- backup.sh
+++ backup.sh
@@ -15,7 +15,8 @@ help(){
 echo "Options:"
 echo "-h help"
 echo "-c compress backup"
-echo "-p preserve folders structure"
+echo "-p preserve realtive path"
+echo "-k preserve absolute path"
 echo "-d backup destination"
 echo "-f files to include"
 echo "-m modification date ('+' - greater than, '-' - less than, '' - exactly) "
@@ -24,15 +25,22 @@ echo "-o owner"
 }
 
 preserve_struct=false
+preserve_abs_struct=false
 
-while getopts :hvpcm:s:o:d:f: OPTION
+while getopts :hvpkcm:s:o:d:f: OPTION
 do
 	case $OPTION in
-		h ) help ;;
+		h ) help; exit ;;
 		c ) exit 1;;
 		p ) preserve_struct=true ;;
+		k ) preserve_abs_struct=true ;;
 		d ) dest_folder="$OPTARG/backup" ;;
-		f ) files+=("$OPTARG") ;;
+		f ) if [ -d $OPTARG ]; then
+			    file="$(cd -- "$OPTARG" && pwd)"
+		    else    
+			    file="$(cd -- "$(dirname "$OPTARG")" && pwd)/$(basename "$OPTARG")"
+		    fi 
+		    files+=("$file") ;; 	# Get abolute path 
 		m ) last_modified="-mtime $OPTARG";;
 		s ) size="-size $OPTARG"."k";;
 		o ) owner="-user $OPTARG" ;;
@@ -49,19 +57,40 @@ if [ -z "$dest_folder" ]; then
 	exit 1
 fi
 
-# creates folder if it doesn't exist
-mkdir -p "$dest_folder"	
-
-if $preserve_struct ; then
-	copy_opt="--parents"
-	dest_folder=$(cd -- "$dest_folder" && pwd) # Makes it absolute path 
+mkdir -p "$dest_folder" # creates folder if it doesn't exist
+dest_folder=$(cd -- "$dest_folder" && pwd) # Makes it absolute path 
 
 # A double dash (--) is used in commands to signify the end of
 # command options, so files containing dashes or other special
 # characters won't break the command.
+
+if $preserve_abs_struct ; then
+	for file in "${files[@]}"; do
+		if [ -d $file ]; then
+			find $file $last_modified $size $owner -exec cp 2> /dev/null --parents {} "$dest_folder"  \;
+		else
+			path="$(dirname $file)"
+			mkdir -p "$dest_folder/$path"
+
+			find $file $last_modified $size $owner -exec cp 2> /dev/null {} "$dest_folder/$path" \;
+		fi
+	done
+elif $preserve_struct ; then
+	for file in "${files[@]}"; do
+		if [ -d $file ]; then		
+			base_dir="$(basename $file)"
+			mkdir -p "$dest_folder/$base_dir"
+
+	        	cd -- $file && 
+			find . $last_modified $size $owner -exec cp 2> /dev/null --parents {} "$dest_folder/$base_dir"  \;
+		else 	
+			find $file $last_modified $size $owner -exec cp 2> /dev/null {} "$dest_folder"  \;
+		fi
+	done
+else
+	for file in "${files[@]}"; do
+		find $file $last_modified $size $owner -exec cp 2> /dev/null {} "$dest_folder" \;
+	done
 fi
 
-for file in "${files[@]}"; do
-	cd -- "$file" &&
-	find . $last_modified $size $owner -exec cp 2> /dev/null $copy_opt {} "$dest_folder"  \;	
-done
+	
